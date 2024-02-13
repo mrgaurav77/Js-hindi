c9d3f1c7e279d74172468524f622b2eb76cf7a43
Gaurav Krushna Kolhe <105879073+mrgaurav77@users.noreply.github.com>
2024-02-13 19:23:49 +0000

    Conversion of deta type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)  // Conversition
+//console.log(typeof valueInNumber);
+
+
+//"33"=> 33
+// "33abc" => NaN // full form of NaN is Not a number and its type is number
+// true => 1 ; falsev => 0
+// Null => 0
+// undefined => NaN 
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+// 1 => true ; 0 => false 
+// "" => false 
+// "Gaurav" => true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
